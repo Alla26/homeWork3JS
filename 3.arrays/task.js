01a61eaad7a78b1ea6933a2e5e0454bc9f982008
unknown <girlalla@yandex.ru>
2022-11-04 15:33:59 +0300

    Initial commit

---
+++ 3.arrays/task.js
@@ -0,0 +1,16 @@
+function compareArrays(arr1, arr2) {
+  let result;
+  if (arr1.length === arr2.length) {
+    return (result = arr1.every((value, i) => value === arr2[i]));
+  }
+  return false;
+}
+
+function advancedFilter(arr) {
+  let resultArr;
+  positiveArr = arr.filter((element) => element > 0);
+  multipleOfThreeArr = positiveArr.filter((element) => element % 3 === 0);
+  resultArr = multipleOfThreeArr.map((element) => element * 10);
+  return resultArr;
+}
+//return arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
