067ec922ca3d2bebcffde302f9bd918838b8e2a5
unknown <girlalla@yandex.ru>
2022-11-05 16:20:47 +0300

    Учла корректировки преподавателя

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,16 +1,20 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  /* let result;
   if (arr1.length === arr2.length) {
     return (result = arr1.every((value, i) => value === arr2[i]));
   }
-  return false;
+  return false;*/
+  return arr1.length === arr2.length && arr1.every((value, i) => value === arr2[i]);
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  /*let resultArr;
   positiveArr = arr.filter((element) => element > 0);
   multipleOfThreeArr = positiveArr.filter((element) => element % 3 === 0);
   resultArr = multipleOfThreeArr.map((element) => element * 10);
-  return resultArr;
+  return resultArr;*/
+  return arr
+    .filter((element) => element > 0)
+    .filter((element) => element % 3 === 0)
+    .map((element) => element * 10);
 }
-//return arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
